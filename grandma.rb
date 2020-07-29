b71a9ff138b97d4a1b83a22da0233844c043d59d
Kelin Field <kfield111@hotmail.com>
2020-07-29 19:34:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,9 @@
 def speak_to_grandma (string)
   if string == "I LOVE YOU GRANMDA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
+elsif string == string.upcase
+  return "NO, NOT SINCE 1938!"
+else
+  return "HUH?! SPEAK UP, SONNY!"
 end
 end
